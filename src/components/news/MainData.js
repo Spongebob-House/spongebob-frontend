9e65fc2f829242900e2041214ff579800a125988
yezi <yejison98@gmail.com>
2022-11-21 23:23:49 +0900

    feat: news/card 하나씩 + 두개씩

---
+++ src/components/news/MainData.js
@@ -0,0 +1,11 @@
+const MainData = {
+  cnt: 1,
+  description:
+    '20년간 살던 작은 방을 벗어나 저렴한 보증금의 공공임대 <b>주택</b>으로 이사하려던 계획이 물거품이 됐었기 때문이다.정씨가 지난 2017년 신청했던 &apos;주거 상향 지원&apos;은 각 지자체와 LH가 협력해 쪽방, 고시원, 반지하 등 비<b>주택</b>... ',
+  link: 'http://www.kyongbuk.co.kr/news/articleView.html?idxno=2117303',
+  org_link: 'http://www.kyeongin.com/main/view.php?key=20221116010002982',
+  pDate: '2022-11-17 20:48:00',
+  title: '포스코건설, 동반성장지원단 활동 우수 협력사 포상',
+  src: 'http://imgnews.naver.net/image/5119/2017/06/16/0000598385_001_20170616060324716.png',
+};
+export default MainData;
--- src/views/AppNews.vue
+++ src/views/AppNews.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <h2>Latest News</h2>
+    <h2>News</h2>
+
+    <!-- card item 가져와서 뿌리기-->
+    <div class="card-one-container">
+      <div class="card-one">
+        <a :href="`${onedata.link}`" target="_blank">
+          <div class="card-one-item">
+            <div class="card-one-img">
+              <img :src="`${onedata.src}`" alt="" />
+            </div>
+            <div class="card-one-right">
+              <div v-html="onedata.title" class="card-one-title"></div>
+              <div class="card-one-pdate">{{ onedata.pDate }}</div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <!-- 두개씩 -->
+    <div class="card-few-container">
+      <div v-for="(news, index) in fewdata" :key="index">
+        <a :href="`${news.link}`" target="_blank">
+          <div class="card-few-item">
+            <div class="card-few-img">
+              <img :src="`${news.src}`" style="width: 100%; height: 100%" alt="" />
+            </div>
+            <div v-html="news.title" class="card-few-title"></div>
+            <div class="card-few-pdate">{{ news.pDate }}</div>
+          </div>
+        </a>
+      </div>
+    </div>
 
     <div class="card-container">
-      <!-- card item 가져와서 뿌리기-->
       <div v-for="(news, index) in newsdata" :key="index">
-        <a :href="`${news.src}`" target="_blank">
+        <a :href="`${news.link}`" target="_blank">
           <div class="card-item">
-            <div class="card-img"><img :src="`${news.thumbnail}`" style="width: 100%; height: 12rem" alt="" /></div>
+            <div class="card-img">
+              <img :src="`${news.src}`" style="width: 100%; height: 12rem" alt="" />
+            </div>
             <div v-html="news.title" class="card-title"></div>
             <div class="card-pdate">{{ news.pDate }}</div>
           </div>
@@ -18,12 +51,16 @@
 </template>
 
 <script>
-import data from "@/components/news/NewsData";
+import data from '@/components/news/NewsData';
+import oneData from '@/components/news/MainData';
+import fewData from '@/components/news/FewData';
 export default {
-  name: "AppNews",
+  name: 'AppNews',
   data() {
     return {
       newsdata: data,
+      onedata: oneData,
+      fewdata: fewData,
     };
   },
   created() {
@@ -34,11 +71,104 @@ export default {
 
 <style>
 h2 {
-  font-family: Inter;
+  font-family: Spoqa Han Sans Neo;
+  font-weight: bold;
 }
 a {
   text-decoration: none !important;
 }
+
+/* 하나짜리 카드  */
+.card-one-container {
+  width: 90%;
+  margin: 50px auto;
+  /* background-color: yellow; */
+}
+
+.card-one-item {
+  width: 81rem;
+  height: 30rem;
+  margin: 0 auto;
+  display: flex;
+  border-radius: 2rem;
+  box-shadow: 5px 5px 30px 0 rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  overflow: hidden;
+}
+.card-one-item:hover {
+  opacity: 0.5;
+}
+.card-one-title {
+  width: 80%;
+  height: 60%;
+  margin: 50px auto;
+  font-family: Spoqa Han Sans Neo;
+  font-size: 40px;
+  font-weight: bold;
+  color: black;
+}
+
+.card-one-pdate {
+  width: 80%;
+  margin: auto;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: Spoqa Han Sans Neo;
+  color: #9d9d9d;
+}
+/* 두개짜리 카드 */
+
+.card-few-container {
+  width: 90%;
+  margin: 50px auto;
+  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(10rem, auto));
+  gap: 3rem;
+}
+
+.card-few-item {
+  width: 39em;
+  height: 35rem;
+  position: relative;
+  flex-direction: column;
+  border-radius: 2rem;
+  box-shadow: 5px 5px 30px 0 rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  overflow: hidden;
+}
+
+.card-few-item:hover {
+  opacity: 0.5;
+}
+.card-few-img {
+  width: 100%;
+  height: 60%;
+  background-color: #9d9d9d;
+}
+
+.card-few-title {
+  width: 85%;
+  height: 5rem;
+  margin: 40px auto;
+  font-family: Spoqa Han Sans Neo;
+  font-size: 28px;
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  overflow: hidden;
+}
+.card-few-pdate {
+  width: 85%;
+  float: bottom;
+  margin: 0px auto;
+  font-family: Spoqa Han Sans Neo;
+  color: #9d9d9d;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+/* 세개 짜리 */
 .card-container {
   width: 90%;
   margin: 50px auto;
@@ -46,8 +176,8 @@ a {
   display: grid;
   grid-template-columns: repeat(3, minmax(10rem, auto));
   gap: 3rem;
+  /* background-color: yellow; */
 }
-
 .card-item {
   width: 25rem;
   height: 20rem;
